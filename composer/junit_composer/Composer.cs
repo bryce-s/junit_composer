e552ac792947c77d41aa8dfec3b68f2122015d77
bryce-s <bryces@umich.edu>
2019-07-31 13:27:06 -0400

    change to net standard

---
+++ composer/junit_composer/Composer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml.Linq;
+
+
+namespace junit_composer
+{
+     public static class Composer
+    {
+
+        private static XDocument OpenXmlFile(string filename)
+        {
+            return XDocument.Load(filename);
+        }
+        
+
+        // combines paramas into a single junit file with multiple test suites.
+        public static string ComposeTestSuites(params string[] targets)
+        {
+
+            return "";
+        }
+
+
+        // combines params into a single junit file, with a suite of test cases.
+        public static string ComposeTestCases(params string[] targets)
+        {
+
+            return "";
+        }
+       
+    }
+}

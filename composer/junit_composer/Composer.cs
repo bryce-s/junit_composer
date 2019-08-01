f76be31e0c64be256b4316a5d7e083481d90e498
bryce-s <bryces@umich.edu>
2019-08-01 16:20:29 -0400

    7 of 7 passing

--- composer/junit_composer/Composer.cs
+++ composer/junit_composer/Composer.cs
@@ -22,6 +22,8 @@ namespace junit_composer
         private static string testsuites = "testsuites";
         private static string unexpected_child_elt = "Unexpected child element found while parsing.";
 
+        private static int test_total, failure_total, error_total = 0;
+
         private static XDocument OpenXmlFile(string filename)
         {
             return XDocument.Load(filename);
@@ -79,12 +81,17 @@ namespace junit_composer
         private static List<XElement> ExtractTestCases(string filename)
         {
             List<XElement> test_objects = new List<XElement>();
-            List<XElement> xelt_list = ExtractTestSuites(filename);
+            List<XElement> test_suite_list = ExtractTestSuites(filename);
+
+            // reset static vars
+            zero_totals();
+
             // we know they're all testsuite objects. We can extract the test cases only
             // and return those. Then manually build the suite and header.
-            foreach (XElement xelt in xelt_list)
+            foreach (XElement test_suite in test_suite_list)
             {
-                test_objects.AddRange(xelt.Descendants("testcase"));
+                test_objects.AddRange(test_suite.Descendants("testcase"));
+                log_tests_failures_errors(test_suite);
             }
             return test_objects;
         }
@@ -108,29 +115,75 @@ namespace junit_composer
             return suite_enumerator.Current;
         }
 
-        private static XDocument set_up_junit_document()
+        private static XDocument set_up_junit_document(bool testsuites_b = true)
         {
             XDocument res_doc = new XDocument(
                 new XDeclaration("1.0", "utf-8", "yes")
                 );
-            res_doc.Add(new XElement(testsuites));
+            if (testsuites_b)
+            {
+                res_doc.Add(new XElement(testsuites));
+            }
             return res_doc;     
         }
 
 
         // returns a testsuite xelement
-        private static XElement add_single_testsuite(XDocument res_doc)
+        private static XElement add_single_testsuite(XDocument xdoc)
         {
-            // we grab the testsuites elt:
-            XElement testsuites_obj = return_xelement(xdoc: res_doc);
-            testsuites_obj.Add(new XElement(testsuite));
+            xdoc.Add(new XElement(testsuite));
+            IEnumerator<XElement> testsuite_enumerator = xdoc.Descendants(testsuite).GetEnumerator();
+            testsuite_enumerator.MoveNext();
 
-            //we greab the testsuite elt:
-            XElement testsuite_obj = return_xelement(xelt: testsuites_obj);
-            return testsuite_obj;
+
+
+            XElement suite = testsuite_enumerator.Current;
+            return suite;
         }
 
-        private static XDocument build_test_suite_only(List<XElement> test_cases)
+
+        private static void zero_totals()
+        {
+            test_total = 0;
+            failure_total = 0;
+            error_total = 0;
+        }
+
+        private static void log_tests_failures_errors(XElement testsuite)
+        {
+            XAttribute tests = testsuite.Attribute("tests");
+            XAttribute failures = testsuite.Attribute("failures");
+            XAttribute errors = testsuite.Attribute("errors");
+            try
+            {
+                if (tests != null)
+                {
+                    test_total += Convert.ToInt32(tests.Value.ToString());
+                }
+                if (errors != null)
+                {
+                    error_total += Convert.ToInt32(errors.Value.ToString());
+                }
+                if (failures != null)
+                {
+                    failure_total += Convert.ToInt32(failures.Value.ToString());
+                }
+            }
+            catch (InvalidCastException e)
+            {
+                throw (new BadJunitFileException("testsuite element attributes for tests, value, error must be integers."));
+            }
+        } 
+
+        // testsuite or testsuites works on param
+        static private void add_attributes(XElement test_suite_obj)
+        {
+            test_suite_obj.Add(new XAttribute("tests", test_total.ToString()));
+            test_suite_obj.Add(new XAttribute("failures", failure_total.ToString()));
+            test_suite_obj.Add(new XAttribute("errors", error_total.ToString()));
+        }
+        
+        private static XDocument build_test_suites(List<XElement> test_cases)
         {
             XDocument res_doc = set_up_junit_document();
 
@@ -139,53 +192,31 @@ namespace junit_composer
             testsuites_iter.MoveNext();
             XElement testsuites_obj = testsuites_iter.Current;
 
-            int total_test_count = 0;
-            int total_failure_count = 0;
-            int total_error_count = 0;
-
+            zero_totals();
             // add all testsuite element to the testsuites:
             foreach (XElement testsuite in test_cases)
             {
                 testsuites_obj.Add(testsuite);
-                XAttribute tests = testsuite.Attribute("tests");
-                XAttribute failures = testsuite.Attribute("failures");
-                XAttribute errors = testsuite.Attribute("errors");
-                try
-                {
-                    if (tests != null)
-                    {
-                        total_test_count += Convert.ToInt32(tests.Value.ToString());
-                    }
-                    if (errors != null)
-                    {
-                        total_error_count += Convert.ToInt32(errors.Value.ToString());
-                    }
-                    if (failures != null)
-                    {
-                        total_failure_count += Convert.ToInt32(failures.Value.ToString());
-                    }
-                } catch (InvalidCastException e)
-                {
-                    throw (new BadJunitFileException("testsuite element attributes for tests, value, error must be integers."));
-                }
+                log_tests_failures_errors(testsuite);
+             
             }
-            testsuites_obj.Add(new XAttribute("tests", total_test_count.ToString()));
-            testsuites_obj.Add(new XAttribute("failures", total_failure_count.ToString()));
-            testsuites_obj.Add(new XAttribute("errors", total_error_count.ToString()));
+            add_attributes(testsuites_obj);
             return res_doc;
         }
 
-        private static XDocument build_test_suite_document(List<XElement> test_suites)
+        private static XDocument build_test_suite(List<XElement> test_cases)
         {
-            XDocument res_doc = set_up_junit_document();
+            XDocument res_doc = set_up_junit_document(testsuites_b: false);
+            add_single_testsuite(res_doc);
             IEnumerator<XElement> test_suite_elt = res_doc.Descendants(testsuite).GetEnumerator();
             test_suite_elt.MoveNext();
             XElement test_suite = test_suite_elt.Current;
-            foreach (XElement suite in test_suites)
+            foreach (XElement tc in test_cases)
             {
-                test_suite.Add(suite);
+                test_suite.Add(tc);
                 
             }
+            add_attributes(test_suite);
             return res_doc;
         }
 
@@ -218,7 +249,8 @@ namespace junit_composer
             {
                 test_suites.AddRange(ExtractTestSuites(filename: target));
             }
-            XDocument test_suite_doc = build_test_suite_only(test_suites);
+            XDocument test_suite_doc = build_test_suites(test_suites);
+       
             return append_encoding(test_suite_doc);
         }
 
@@ -237,7 +269,7 @@ namespace junit_composer
             {
                 test_cases.AddRange(ExtractTestCases(target));
             }
-            XDocument test_case_doc = build_test_suite_only(test_cases);
+            XDocument test_case_doc = build_test_suite(test_cases);
 
             return append_encoding(test_case_doc);
         }
---
+++ test/test.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using NUnit.Framework;
+using junit_composer;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace test
+{
+    [TestFixture]
+    public class Test
+    {
+
+        private string spec_example = "spec_example.junit";
+
+        private string mode_cases = "cases";
+        private string mode_suites = "suites";
+
+        static private string get_test_cwd()
+        {
+            return TestContext.CurrentContext.TestDirectory;
+        }
+
+        static private string build_filename(string test_target)
+        {
+            string context = get_test_cwd();
+            string test_target_path = String.Format("test_files{0}spec_example.junit", Path.DirectorySeparatorChar);
+            return Path.Combine(context, test_target_path);
+        }
+
+        static private void write_result_to_file(string content, string filename = "out.junit")
+        {
+            string context = get_test_cwd();
+            string path_target = Path.Combine(context, filename);
+            if (File.Exists(path_target))
+            {
+                File.Delete(path_target);
+            }
+            using (StreamWriter sw = File.CreateText(path_target))
+            {
+                sw.Write(content);
+            }
+
+        }
+
+        static private void match_tag(string tag, string input)
+        {
+            string pattern = String.Format(@"<{0}[\s\S]+<\/{0}>", tag);
+            Match m = Regex.Match(input, pattern, RegexOptions.ECMAScript);
+            Assert.True(m.Success);
+        }
+
+        
+        static private string read_correct_file(string filename, string mode)
+        {
+            string context = get_test_cwd();
+            string target = String.Format("correct{0}{1}-{2}.correct", Path.DirectorySeparatorChar, filename, mode);
+            string target_path = Path.Combine(context, target);
+            return File.ReadAllText(target_path);
+        }
+
+        // returns true if strings do not differ.
+        static private bool diff_stings(string lhs_result, string rhs_result)
+        {
+            return lhs_result == rhs_result;
+        }
+
+        void match_main_tags(string res)
+        {
+            match_tag("testsuite", res);
+            match_tag("testsuites", res);
+            match_tag("testcase", res);
+        }
+
+        // Tests below:
+
+
+        [Test]
+        public void log_correct_tests()
+        {
+            bool correct = false;
+            if (correct)
+            {
+                string suites_res = Composer.ComposeTestSuites(build_filename(spec_example));
+                string cases_res = Composer.ComposeTestCases(build_filename(spec_example));
+                string cases_target = String.Format("correct{0}{1}-{2}.correct", Path.DirectorySeparatorChar, spec_example, mode_cases);
+                string suites_target = String.Format("correct{0}{1}-{2}.correct", Path.DirectorySeparatorChar, spec_example, mode_suites);
+                write_result_to_file(cases_res, cases_target);
+                write_result_to_file(suites_res, suites_target);
+
+            }
+            Assert.False(correct);
+
+        }
+
+
+        [Test]
+        public void suites_spec_tags_present()
+        {
+            string res = Composer.ComposeTestSuites(build_filename(spec_example));
+            write_result_to_file(content: res);
+            match_main_tags(res);
+        }
+
+        [Test]
+        public void suites_spec_diff()
+        {
+            string res = Composer.ComposeTestSuites(build_filename(spec_example));
+            string correct_string_unsanitized = read_correct_file(spec_example, mode_suites);
+            Assert.True(diff_stings(res, correct_string_unsanitized)); 
+        }
+
+        [Test]
+        public void suites_contains_tests_failures_errors()
+        {
+            string res = Composer.ComposeTestSuites(build_filename(spec_example));
+            Assert.True(res.Contains("<testsuites tests=\"45\" failures=\"17\" errors=\"0\">"));
+        }
+
+
+        [Test]
+        public void cases_spec_tags_present()
+        {
+            string res = Composer.ComposeTestCases(build_filename(spec_example));
+            write_result_to_file(res);
+            match_tag("testsuite", res);
+            match_tag("testcase", res);
+        }
+
+        [Test]
+        public void cases_contains_test_failures_errors()
+        {
+            string res = Composer.ComposeTestCases(build_filename(spec_example));
+            Assert.True(res.Contains("<testsuite tests=\"45\" failures=\"17\" errors=\"0\">"));
+
+        }
+
+        [Test]
+        public void cases_spec_diff()
+        {
+            string res = Composer.ComposeTestCases(build_filename(spec_example));
+            string correct_string_unsanitized = read_correct_file(spec_example, mode_cases);
+            Assert.True(diff_stings(res, correct_string_unsanitized));
+        }
+
+
+    }
+}
